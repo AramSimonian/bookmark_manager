0b9feeef7756399a2776ed00348bc6f70c25c0d3
Aram Simonian <aram.contacts@gmail.com>
2017-10-19 15:35:20 +0100

    Changed name of link create button to Submit;

--- spec/features/web_helper.rb
+++ spec/features/web_helper.rb
@@ -3,5 +3,6 @@ def create_new_link
   visit('/links/new')
   fill_in :title, with: 'BBC'
   fill_in :URL, with: 'http://www.bbc.co.uk'
-  click_button 'Create Link'
+  fill_in :tag, with: 'kittens'
+  click_button 'Submit'
 end
