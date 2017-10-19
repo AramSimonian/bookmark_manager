41ea98b141fc95ba812d734a7fd7cd10b9897986
Aram Simonian <aram.contacts@gmail.com>
2017-10-19 15:31:46 +0100

    Refactored DataMapper functionality to data_mapper_setup

---
+++ app/data_mapper_setup.rb
@@ -0,0 +1,8 @@
+require 'data_mapper'
+require './app/models/link'
+require './app/models/tag'
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] ||
+      "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+DataMapper.finalize
+DataMapper.auto_upgrade!
